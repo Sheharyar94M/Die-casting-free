7fe0285e251615cd82111ebd58cfaaac5e5a7576
Muhammad Sheharyar <sheharyar.masoodpro@gmail.com>
2024-10-06 10:59:59 +0500

    Die-casting_free

---
+++ app/src/main/java/com/example/metallurgy_free_ads/PdfDetail.java
@@ -0,0 +1,17 @@
+package com.example.metallurgy_free_ads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.qsa.die_casting_free.R;
+
+public class PdfDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_detail);
+
+    }
+}
---
+++ app/src/main/java/com/qsa/die_casting_free/Interfaces/FragmentCallback.java
@@ -0,0 +1,5 @@
+package com.qsa.die_casting_free.Interfaces;
+
+public interface FragmentCallback {
+    void doSomething();
+}
